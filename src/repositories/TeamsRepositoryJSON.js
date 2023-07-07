70b79ef71af3199b1398ed47159410149a57b9ca
daniNKV <nikiforovdaniel@gmail.com>
2023-07-06 21:18:49 -0300

    Error handling

--- src/repositories/TeamsRepositoryJSON.js
+++ src/repositories/TeamsRepositoryJSON.js
@@ -7,15 +7,20 @@ module.exports = class TeamsRepositoryJSON {
     }
 
     getAll() {
-        return this.readFileData().map((item) => new Team(item));
+        const data = this.readFileData();
+        if (!data) throw new Error('Invalid JSON file');
+        return data.map((item) => new Team(item));
     }
 
     getById(id) {
         const data = this.readFileData();
-        return data.find((item) => item.id === id);
+        const rawTeam = data.find((item) => item.id === id);
+        if (!rawTeam) throw new Error('Team not found');
+        return new Team({ rawTeam });
     }
 
     add(team) {
+        if (this.getById(team.id)) throw new Error('Team already exists');
         const data = this.readFileData();
         data.push(team);
         this.writeFileData(data);
@@ -24,24 +29,33 @@ module.exports = class TeamsRepositoryJSON {
     update(team) {
         const data = this.readFileData();
         const index = data.findIndex((item) => item.id === team.id);
+        if (index === -1) throw new Error('Team not found');
         data[index] = team;
         this.writeFileData(data);
-    }
-
-    readFileData() {
-        const fileData = fs.readFileSync(this.path, 'utf8');
-        return JSON.parse(fileData);
-    }
-
-    writeFileData(data) {
-        const fileData = JSON.stringify(data, null, 2);
-        fs.writeFileSync(this.path, fileData, 'utf8');
     }
 
     delete(id) {
         const data = this.readFileData();
         const index = data.findIndex((item) => item.id === id);
-        data.splice(index, 1);
-        this.writeFileData(data);
+        if (index === -1) throw new Error('Team not found');
+        this.writeFileData(data
+            .slice(0, index)
+            .concat(data.slice(index + 1)));
+    }
+
+    readFileData() {
+        const fileData = fs.readFileSync(this.path, 'utf8');
+        let data;
+        try {
+            data = JSON.parse(fileData);
+        } catch (e) {
+            throw new Error('Invalid JSON file');
+        }
+        return data;
+    }
+
+    writeFileData(data) {
+        const fileData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(this.path, fileData, 'utf8');
     }
 };
